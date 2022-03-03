f17fe996820b9a6fb3eaa0e4c63218bd97e24584
Bwogi Andrew <andrew.bwogi@gmail.com>
2022-03-02 16:23:41 -0800

    placeholder and random api practice

---
+++ api-learn.js
@@ -0,0 +1,45 @@
+// placeholder photos
+
+const placeholderPhotos = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/photos');
+  const data = res.json();
+  console.log(data);
+};
+placeholderPhotos();
+
+// find placeholder posts
+const placeholderPosts = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  console.log(data);
+};
+placeholderPosts();
+
+// find placeholder todos
+
+const placeholderTodos = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+  const data = await res.json();
+  console.log(data);
+};
+placeholderTodos();
+
+// find a random user
+// you have that api('https://randomuser.me/api') fetch that data and console log it.
+// RandomData();
+// RandomData();
+// RandomData();
+
+// async function RandomData() {
+//   const res = await fetch('https://randomuser.me/api');
+//   const data = res.json();
+//   console.log(data);
+// }
+
+const randomUser = async () => {
+  const res = await fetch('https://randomuser.me/api');
+  // console.log(res);
+  const data = await res.json();
+  console.log(data);
+};
+randomUser();
