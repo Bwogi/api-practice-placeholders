85251394036cfb0325b5846fab77c17ec550bb9c
Bwogi Andrew <andrew.bwogi@gmail.com>
2022-03-03 20:13:35 -0800

    structural adjustment

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const randomUser = async () => {
   const res = await fetch('https://randomuser.me/api');
   const data = await res.json();
-
+  // console.log(data);
   const user = data.results[0];
 
   const newUser = {
