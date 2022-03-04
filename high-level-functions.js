31536416ee213fd482811aa297ef392224ecab48
Bwogi Andrew <andrew.bwogi@gmail.com>
2022-03-03 19:34:57 -0800

    extract data from object

--- high-level-functions.js
+++ high-level-functions.js
@@ -1,6 +1,15 @@
 const randomUser = async () => {
   const res = await fetch('https://randomuser.me/api');
   const data = await res.json();
+
+  const user = data.results[0];
+
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000),
+  };
+  console.log(newUser);
+
   // console.log(data);
   // console.log(data.results[0].name);
   // console.log(data.results[0].dob.date);
@@ -12,16 +21,20 @@ const randomUser = async () => {
   // console.log(data.results[0].name.first);
   // console.log(data.results[0].name.last);
 
-  const user = data.results[0];
-  const address = user.location;
-  const location = address.coordinates;
+  // const user = data.results[0];
+  // const address = user.location;
+  // const location = address.coordinates;
 
-  console.log(address);
-  console.log(address.city);
-  console.log(address.state);
-  console.log(location);
-  console.log(location.latitude);
-  console.log(location.longitude);
+  // console.log(address);
+  // console.log(address.city);
+  // console.log(address.state);
+  // console.log(location);
+  // console.log(location.latitude);
+  // console.log(location.longitude);
 };
 
 randomUser();
+randomUser();
+randomUser();
+// randomUser();
+// randomUser();
